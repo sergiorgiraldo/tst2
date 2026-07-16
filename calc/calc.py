589a0d1d2de784752de14d62ab92a0836a857f33
Sergio Giraldo <sergio@giraldo.com.br>
2026-07-16 19:20:32 +0200

    feat: multipliction to test entire ::by sergio giraldo @ 20260716T1920CEST, gpg signed

--- calc/calc.py
+++ calc/calc.py
@@ -6,8 +6,13 @@ def subtract(a, b):
     return a - b
 
 
+def multiply(a, b):
+    return a * b
+
+
 if __name__ == "__main__":
     x = float(input("Enter first number: "))
     y = float(input("Enter second number: "))
     print("Add result:", add(x, y))
     print("Subtract result:", subtract(x, y))
+    print("Multiply result:", multiply(x, y))
